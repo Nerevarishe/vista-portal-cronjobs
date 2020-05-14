083383a53cb1687a57e2cc729974dc820ea42902
Nerevarishe <JIyHaTuK@gmail.com>
2020-05-14 13:59:20 +0300

    create backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,6 +2,21 @@
 
 #vars
 DATETIME=$(date +%d-%m-%Y.%H-%M-%S)
-
+PATH=$(pwd)/backups/${DATETIME}
 # Create backup dir
-mkdir -p ./backups/$DATETIME
+mkdir -p ${PATH}
+
+# Add db dump to backup dir
+mongodump --host db -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} -o ${PATH}/db-backup-${DATETIME}
+
+
+# Add uploads data to backup dir
+mkdir -p ${PATH}/uploads-backup-${DATETIME}
+cp /mnt/backend-uploads/* ${PATH}/uploads-backup-${DATETIME}/
+
+
+# Archivate backup dir and delete original dir
+tar -zcvf $(pwd)/backups/backup-${DATETIME}.tar.gz ${PATH}
+rm -r ${PATH}
+
+# Send archive to backups
